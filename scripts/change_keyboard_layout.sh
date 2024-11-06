e4a94db636046555e72deb3892e6b5df2974e482
samuelberntzen <samuel.berntzen@protonmail.com>
2024-11-06 10:20:15 +0100

    update

--- scripts/change_keyboard_layout.sh
+++
@@ -1,5 +0,0 @@
-# Set keyboard layout to Norwegian
-sudo sed -i '/^XKBLAYOUT=/s/".*"/"no"/' /etc/default/keyboard
-
-# Reconfigure keyboard-configuration
-sudo dpkg-reconfigure -f noninteractive keyboard-configuration
